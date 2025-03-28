cb78f0a3a9e4a0b70c654e4716b6d348f5baadaf
jayshankar <jayshankaryadav435@gmail.com>
2025-03-28 16:57:44 +0530

    adding backendjob

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import connectDB from './database/database.js';
+import jobSeekerRouter from './routes/jobSeekerRoute.js';
+import RecruiterRouter from './routes/recruiterRoute.js';
+import cors from "cors";
+import jobRoute from './routes/jobRoute.js';
+import Applicationrouter from './routes/applicationRoutes.js';
+import LoginRouter from './routes/LoginRoute.js';
+import ProviderRouter from './routes/ProviderRoute.js';
+
+dotenv.config();
+const app = express();
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const PORT = process.env.PORT || 5000;
+app.use(express.json()); 
+app.use('/api/user',jobSeekerRouter)
+app.use('/api/recruiter',RecruiterRouter)
+app.use('/api/job',jobRoute)
+app.use('/api/apply',Applicationrouter)
+app.use('/api/auth',LoginRouter)
+app.use('/api/authprovider',ProviderRouter)
+
+// Sample Route
+app.get('/', (req, res) => {
+    res.send('Database Server is Running');
+});
+// Start Server
+app.listen(PORT, () => {
+    connectDB()
+    console.log(`Server running on port ${PORT}`);
+});
